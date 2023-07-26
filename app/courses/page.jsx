ea034e935fad5976a2ba6d06c6c565308a90c85a
Sumit Maithani <sumitmaithani824@gmail.com>
2023-07-26 21:37:24 +0530

    added new cp gap feature

--- app/courses/page.jsx
+++ app/courses/page.jsx
@@ -2,6 +2,7 @@
 
 import RemoveBtnCourse from "../../components/RemoveBtnCourse";
 import Image from "next/image";
+import { BsInfoCircleFill } from "react-icons/bs";
 
 const getCourses = async () => {
   try {
@@ -51,8 +52,27 @@ const CompetencyAnalysis = async (courseSubjects) => {
   } else {
     // Calculate the matching score in the range of 0 to 100
     const matchingScore = (totalMatchingSubjects / totalCourseSubjects) * 100;
+
     return Math.round(matchingScore);
   }
+};
+
+const unmatchedSubjects = async (courseSubjects) => {
+  const { topics } = await getTopics();
+  const matchingSubjects = courseSubjects.filter((subject) =>
+    topics.some((obj) => obj.Subjects.includes(subject))
+  );
+
+  // Find the unmatched subjects
+  const unmatchedSubjects = courseSubjects.filter(
+    (subject) => !matchingSubjects.includes(subject)
+  );
+
+  if (unmatchedSubjects.length == 0) {
+    return "You all good";
+  }
+
+  return unmatchedSubjects+' ';
 };
 
 export default async function page() {
@@ -76,7 +96,29 @@ export default async function page() {
                 <b>Learning Hours : </b>
                 <div>{t.LearningHours}</div>
               </div>
-              <div>CP : {CompetencyAnalysis(t.Subjects)}/100</div>
+              <div className="flex items-center ">
+                <div>CP : {CompetencyAnalysis(t.Subjects)}/100 </div>
+                <div class="group relative cursor-pointer py-2 ml-2">
+                  <div class="absolute invisible bottom-7 group-hover:visible bg-black text-white px-4 mb-3 py-2 text-sm rounded-md">
+                    <p class=" leading-2 text-white-600 pt-2 pb-2">
+                      {unmatchedSubjects(t.Subjects)}
+                    </p>
+                    <svg
+                      class="absolute z-10  bottom-[-10px] "
+                      width="16"
+                      height="10"
+                      viewBox="0 0 16 10"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M8 10L0 0L16 1.41326e-06L8 10Z" fill="black" />
+                    </svg>
+                  </div>
+                  <span class="underline hover:cursor-pointer">
+                    <BsInfoCircleFill />
+                  </span>
+                </div>
+              </div>
               <div className="flex items-center text-center">
                 <div className="tag-containerless">
                   {t.Subjects.map((tag, index) => {
