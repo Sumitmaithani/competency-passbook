90d9b7e2c52f41eb7accc759d905864859be097b
Sumit Maithani <sumitmaithani824@gmail.com>
2023-08-13 16:43:17 +0530

    resolved deployment issue

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,6 @@
-"use client";
-
 import Navbar from "@/components/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
-import { ThemeProvider } from "@material-tailwind/react";
 import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,13 +14,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider>
-          <div>
-            <Navbar />
-            <div>{children}</div>
-            <Footer className="mt-15" />
-          </div>
-        </ThemeProvider>
+        <div>
+          <Navbar />
+          <div>{children}</div>
+          <Footer className="mt-15" />
+        </div>
       </body>
     </html>
   );
